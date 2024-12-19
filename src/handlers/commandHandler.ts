52f61c915bedbb504896b2b507b13dce9b3e4cbe
Arootsy <aronrootsaert07@gmail.com>
2024-12-19 16:25:18 +0100

    feat(handlers): added command handler

---
+++ src/handlers/commandHandler.ts
@@ -0,0 +1,35 @@
+import { Command } from '../interfaces/command';
+import fs from 'fs';
+import path from 'path';
+import logger from '../utils/logger';
+
+const commands = new Map<string, Command>();
+
+function loadCommands(directory: string, isRoot = true): number {
+  const commandFiles = fs.readdirSync(directory);
+
+  let commandCount = 0;
+
+  for (const file of commandFiles) {
+    const fullPath = path.join(directory, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      commandCount += loadCommands(fullPath, false);
+    } else if (file.endsWith('.ts') || file.endsWith('.js')) {
+      if (file === 'index.ts' || file === 'index.js') continue;
+
+      const commandModule = require(fullPath);
+      const command: Command = commandModule.default;
+      commands.set(command.data.name, command);
+      commandCount++;
+    }
+  }
+
+  if (isRoot) {
+    logger.info(`${commandCount} command(s) loaded`);
+  }
+  return commandCount;
+}
+
+const totalCommandsLoaded = loadCommands(path.join(__dirname, '../commands'));
+
+export default commands;

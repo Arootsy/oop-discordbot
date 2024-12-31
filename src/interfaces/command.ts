b1f12fed9893906118bc7d2ecc94f69a138f258b
Arootsy <aronrootsaert07@gmail.com>
2024-12-31 12:32:35 +0100

    feat(command): interface added for handler

---
+++ src/interfaces/command.ts
@@ -0,0 +1,17 @@
+import {
+    ChatInputCommandInteraction,
+    AutocompleteInteraction,
+    SlashCommandBuilder,
+    SlashCommandSubcommandsOnlyBuilder,
+    SlashCommandOptionsOnlyBuilder,
+    CacheType,
+} from 'discord.js';
+  
+export interface Command {
+    data:
+      | Omit<SlashCommandBuilder, 'addSubcommandGroup' | 'addSubcommand'>
+      | SlashCommandSubcommandsOnlyBuilder
+      | SlashCommandOptionsOnlyBuilder;
+    run?: (interaction: ChatInputCommandInteraction<CacheType>) => Promise<void>;
+    autocomplete?: (interaction: AutocompleteInteraction<CacheType>) => Promise<void>;
+}
